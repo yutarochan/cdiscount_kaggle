56b74b57e7cf8e9b92f81466665b207531468ddf
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-10-18 20:31:04 -0400

    (add) Append data files

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 # Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 
 echo "Setup Dataset Files"
+mkdir -p Data/
+# wget -x --load-cookies cookies.txt -P . -nH --cut-dirs=5 https://www.kaggle.com/c/grupo-bimbo-inventory-demand/download/cliente_tabla.csv.zip
