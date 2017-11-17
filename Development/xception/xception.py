89a0aab6400504d2593852ca4bb2b0cfd863c291
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-11-17 05:23:58 -0500

    (add) Append more callback functions

--- Development/xception/xception.py
+++ Development/xception/xception.py
@@ -4,15 +4,21 @@ Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 '''
 import sys
 import math
+
 import tensorflow as tf
+
 from keras.models import Model
 from keras.optimizers import Adam
+from keras.callbacks import TensorBoard
 from keras.applications.xception import Xception
 from keras.losses import categorical_crossentropy
 from keras.layers import Dense, GlobalAveragePooling2D
 
 sys.path.append('..')
 import util.data as data
+
+# Application Parameters
+filepath="weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
 
 # Model Parameters
 batch_size_pre  = 64
@@ -21,7 +27,7 @@ epochs_fine = 200
 
 def build_model():
     # Initialize Model
-    model = Xception(include_top=False, weights=None)
+    model = Xception(include_top=False, weights='imagenet')
 
     # Append Model Output
     x = model.output
@@ -32,13 +38,20 @@ def build_model():
     preds = Dense(data.CAT_SIZE, activation='softmax')(x)
     model = Model(inputs=model.inputs, outputs=preds)
 
+    # # first: train only the top layers
+    for layer in base_model.layers: layer.trainable = False
+
     return model
+
+# Initialize Callback Methods
+tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False)
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 
 # Initialize Model
 model = build_model()
 
 # Compile Model
-model.compile(optimizer=Adam(lr=1.0e-4), loss=categorical_crossentropy, metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=1.0e-4), loss=categorical_crossentropy, metrics=['accuracy'], callbacks=[tensorboard, checkpoint])
 
 # Perform Learning
 # TODO: Append System Callback Mechanism to Keep Track of Training
