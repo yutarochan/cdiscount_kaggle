fc4b72869faf16e0c6084facd18fa0ecdb77a8c5
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-11-17 05:49:53 -0500

    (update) Fix and finalize for model finetuning operations

--- Development/xception/xception.py
+++ Development/xception/xception.py
@@ -18,48 +18,52 @@ sys.path.append('..')
 import util.data as data
 
 # Application Parameters
-filepath="weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+base_model_wp = "base_model-{epoch:02d}-{val_acc:.2f}.hdf5"
+fine_model_wp = "fine_model-{epoch:02d}-{val_acc:.2f}.hdf5"
 
 # Model Parameters
 batch_size_pre  = 64
 batch_size_fine = 32
+
+epochs_pre  = 10
 epochs_fine = 200
-
-def build_model():
-    # Initialize Model
-    model = Xception(include_top=False, weights='imagenet')
-
-    # Append Model Output
-    x = model.output
-    x = GlobalAveragePooling2D()(x)
-    x = Dense(8192, activation='relu')(x)
-
-    # Build Model
-    preds = Dense(data.CAT_SIZE, activation='softmax')(x)
-    model = Model(inputs=model.inputs, outputs=preds)
-
-    # # first: train only the top layers
-    for layer in base_model.layers: layer.trainable = False
-
-    return model
 
 # Initialize Callback Methods
 tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False)
-checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+chk_pt_1 = ModelCheckpoint(base_model_wp, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+chk_pt_2 = ModelCheckpoint(fine_model_wp, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 
 # Initialize Model
-model = build_model()
+base_model = Xception(include_top=False, weights='imagenet')
 
-# Compile Model
-model.compile(optimizer=Adam(lr=1.0e-4), loss=categorical_crossentropy, metrics=['accuracy'], callbacks=[tensorboard, checkpoint])
+# Append Model Output
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(8192, activation='relu')(x)
 
-# Perform Learning
-# TODO: Append System Callback Mechanism to Keep Track of Training
-history = model.fit_generator(
+# Build Model
+preds = Dense(data.CAT_SIZE, activation='softmax')(x)
+model = Model(inputs=base_model.inputs, outputs=preds)
+
+# Phase 1: Base Model Training
+print('[Phase 1]: Base Model Training')
+for layer in base_model.layers: layer.trainable = False
+model.compile(optimizer=Adam(), loss=categorical_crossentropy, metrics=['accuracy'], callbacks=[tensorboard, chk_pt_1])
+model.fit_generator(
     generator = data.data_generator(data.TRAIN_BSON_PATH, batch_size=batch_size_pre),
-    steps_per_epoch = data.NUM_TRAIN_PROD / batch_size_fine,
-    epochs = epochs_fine,
-    verbose = 1
+    steps_per_epoch = math.ceil(data.NUM_TRAIN_PROD / batch_size_pre),
+    epochs = epochs_pre,
+    verbose=1,
 )
 
-# model.save(os.path.join('./', 'xception.h5'))
+# Phase 2: Train All Layers (with Lower Learning Rate)
+for layer in model.layers: layer.trainable = True
+model.compile(optimizer=Adam(lr=1.0e-4), loss=categorical_crossentropy, metrics=['accuracy'], callbacks=[tensorboard, chk_pt_2])
+model.fit_generator(
+    generator = data.data_generator(data.TRAIN_BSON_PATH, batch_size=batch_size_fine),
+    steps_per_epoch = math.ceil(data.NUM_TRAIN_PROD / batch_size_fine),
+    epochs = epochs_fine,
+    verbose=1,
+)
+
+model.save(os.path.join('./', 'cdiscount_xception.hdf5'))
